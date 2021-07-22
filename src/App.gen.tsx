0390de476a9e7bf388b89d36b14032b59969ce82
INAJIMA Daisuke <inajima@gmail.com>
2021-07-22 17:37:44 +0900

    Add files generated by bsb

---
+++ src/App.gen.tsx
@@ -0,0 +1,21 @@
+/* TypeScript file generated from App.re by genType. */
+/* eslint-disable import/first */
+
+
+import * as React from 'react';
+
+// @ts-ignore: Implicit any on import
+import * as AppBS__Es6Import from './App.bs';
+const AppBS: any = AppBS__Es6Import;
+
+// tslint:disable-next-line:interface-over-type-literal
+export type Props = {};
+
+export const make: React.ComponentType<{}> = AppBS.make;
+
+// tslint:disable-next-line:interface-over-type-literal
+export type $$default_Props = {};
+
+export const $$default: React.ComponentType<{}> = AppBS.default;
+
+export default $$default;
